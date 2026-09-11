d6db4bcfb748d4fdc34d79cba48578e3191b2226
Arushi Sharma <sharma.arushi653@gmail.com>
2026-09-11 18:46:08 +0530

    Update settings.py

--- employ_project/settings.py
+++ employ_project/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ["https://smart-attrition-predictor-production.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
